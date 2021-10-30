00dcec02f2aeb96978e1552bde6795130e1f70e9
K3VRAL <kevinbestkiller@gmail.com>
2021-10-30 17:08:34 +0200

    Updated pyscript and documentation

--- src/server/data/osu.py
+++ src/server/data/osu.py
@@ -15,9 +15,9 @@ def AddingToEnviroment(jsonoutput):
                 output.write(line)
                 continue
             key, value = line.split('=')
-            if key.lower() in jsonoutput:
-                output.write('OAPI_' + key.upper() + '=' + str(jsonoutput[key.lower()]) + '\n')
-                del jsonoutput[key.lower()]
+            if key[:5] == 'OAPI_' and key[5:].lower() in jsonoutput:
+                output.write(key.upper() + '=' + str(jsonoutput[key[5:].lower()]) + '\n')
+                del jsonoutput[key[5:].lower()]
             else:
                 output.write(line)
         for item in jsonoutput:
@@ -26,48 +26,33 @@ def AddingToEnviroment(jsonoutput):
     os.rename(r'.env.output.temp', r'.env')
 
 def OAuthToken():
+    headers = {
+        'Content-Type'  : 'application/json'
+    }
     bodyparams = {
         'client_id'     : os.getenv('OAPI_CLIENT_ID'),
         'client_secret' : os.getenv('OAPI_CLIENT_SECRET'),
-        'code'          : os.getenv('OAPI_CODE'),
-        'grant_type'    : 'authorization_code',
-        'redirect_uri'  : 'http://localhost'
+        'grant_type'    : 'client_credentials',
+        'scope'         : 'public'
     }
-    r = requests.post('https://osu.ppy.sh/oauth/token', data=bodyparams)
+    r = requests.post('https://osu.ppy.sh/oauth/token', headers=headers, data=json.dumps(bodyparams, separators=(',', ':')))
     if r.ok:
         AddingToEnviroment(r.json())
-        # print('Successfully Posted and Recieved')
     else:
         print('Failed Posting OAuthToken: ' + str(r))
-        exit(3)
-
-def RefreshingOAuthToken():
-    bodyparams = {
-        'client_id'     : os.getenv('OAPI_CLIENT_ID'),
-        'client_secret' : os.getenv('OAPI_CLIENT_SECRET'),
-        'grant_type'    : 'refresh_token',
-        'refresh_token' : os.getenv('OAPI_REFRESH_TOKEN')
-    }
-    r = requests.post('https://osu.ppy.sh/oauth/token', data=bodyparams)
-    if r.ok:
-        AddingToEnviroment(r.json())
-        # print('Successfully Posted Refreshed and Recieved')
-    else:
-        print('Failed Posting OAuthToken For Refresh: ' + str(r))
         exit(2)
 
 def RequestingData():
     baseurl = 'https://osu.ppy.sh/api/v2/'
     headers = {
         'Content-Type'  : 'application/json',
-        'Accept'        : 'application/json',
         'Authorization' : os.getenv('OAPI_TOKEN_TYPE') + ' ' + os.getenv('OAPI_ACCESS_TOKEN')
     }
-    r = requests.get(baseurl + 'me/fruits', headers=headers)
+    r = requests.get(baseurl + 'rankings/fruits/country', headers=headers)
     if r.ok:
         # TODO this
         print(r)
-        # print(r.json())
+        print(r.json())
     else:
         print('Failed Requesting Data: ' + str(r))
         exit(1)
@@ -76,12 +61,8 @@ def main(argv):
     if argv[0] == '1':
         RequestingData()
     elif argv[0] == '2':
-        RefreshingOAuthToken()
-    elif argv[0] == '3':
-        # Only do this for debugging purposes/getting a new token, DON'T EVER USE IN ACTUAL WEBSITE
         OAuthToken()
     else:
-        # print('Quitting...')
         exit(0)
 
 if __name__ == '__main__':
